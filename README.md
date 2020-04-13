5b1f51fe6f614be163b768bd5209c64384b23f69
renato <renato.gamici@gmail.com>
2020-04-13 17:00:30 -0300

    Reorganiza pastas

--- README.md
+++ README.md
@@ -1,3 +1,24 @@
 # COVID-19 em Piracicaba
 
+Os dados referentes ao número de casos confirmados, o de mortes, e as informações sobre cada paciente são obtidas do [site da Prefeitura de Piracicaba](piracicaba.sp.gov.br/).
+Embora não haja nenhuma página organizando os dados, eles podem ser extraídos da página com [notícias sobre o coronavírus na cidade](http://www.piracicaba.sp.gov.br/plantao+coronavirus+covid+19.aspx), ou nas [notícias em geral](https://www.piracicaba.sp.gov.br/categoria/principais+noticias.aspx) (com o título "COMUNICADO ...").
+
+## Dados
+
+Os dados extraídos são colocados em um arquivo com a seguinte formatação
+'''
+Data   Tipo   Número   Sexo   Idade  ## Observação
+'''
+
+O campo `Tipo` é usado para marcar casos confirmados (`P`) ou óbitos (`M`).
+Quando não há informações sobre o paciente, os campos `Sexo` e `Idade` são marcados com `--`.
+
+## Gráficos
+
+### Total de Casos e Novos Casos por Dia
+
+O gráfico é gerado pelo script localizado em `scripts/confirmados.py`, e o resultado é salvo na pasta `img`.
+
+![conf](img/confirmados.png)
+
 
--- scripts/confirmados.py
+++ scripts/confirmados.py
@@ -8,19 +8,25 @@ Script que gerá um gráfico com o número de casos confirmados
 import re
 import datetime
 import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
+import math
 
-data_inicial = '20200325'  # referência para eixo x
-arquivo_entrada = 'dados.txt'
+# data_inicial = '20200325'  # referência para eixo x
+arquivo_entrada = '../dados.txt'
 
 
 def scrap():
-    data = [data_inicial]
-    conf = [0]
+    data = []
+    conf = []
     re_pad = re.compile("([0-9]{8}) +P +([0-9]+).*")
     ent = open(arquivo_entrada, 'r')
     dados = ent.read()
     matches = re.findall(re_pad, dados)
     for match in matches:
+        if data == []:
+            data.append(match[0])
+            conf.append(int(match[1]))
+            continue
         if match[0] == data[-1]:
             conf[-1] += int(match[1])
         else:
@@ -30,7 +36,7 @@ def scrap():
 
 
 def acumulados(data, conf):
-    dia = data_inicial
+    dia = data[0]
     acc = [0]
     for i in range(len(data)):
         if data[i] == dia:
@@ -42,7 +48,7 @@ def acumulados(data, conf):
 
 def dias_corridos(data):
     dias = [0]
-    dia_processado = data_inicial
+    dia_processado = data[0]
     dia_conv = datetime.datetime.strptime(dia_processado, "%Y%m%d")
     for dia in data:
         if dia != dia_processado:
@@ -60,13 +66,50 @@ def plot(data, conf):
     dias = dias_corridos(data)
     # plot do total de casos
     color = 'tab:red'
-    ax1.set_xlabel('data')
-    ax1.set_ylabel('Total de casos (log)', color=color)
+    # ax1.set_xlabel('data')
+    ax1.set_ylabel('Total de Casos', color=color)
     ax1.scatter(dias, acc, color=color)
     ax1.tick_params(axis='y', labelcolor=color)
     ax1.set_yscale('log')
-    # definir ticks
-    # marcar valor do último dia
+    ax1.set_ylim(bottom=0.8)
+    # definir ticks e posição das linhas relevantes
+    # eixo x
+    x_ticks = []
+    x_labels = []
+    vlines_x = []
+    vlines_y = []
+    for i in range(len(dias)):
+        if (
+            i == 0
+            or i == len(dias) - 1
+            or int(data[i]) % 100 == 1
+            or int(data[i]) % 100 == 15
+        ):
+            x_ticks.append(dias[i])
+            x_labels.append(data[i][6:] + '/' + data[i][4:6])
+            vlines_x.append(dias[i])
+            vlines_y.append(acc[i])
+    for x, y in zip(vlines_x, vlines_y):
+        label = str(y)
+        plt.annotate(label, (x, y), textcoords="offset points",
+                     xytext=(10, -10), ha='center', color=color)
+    plt.xticks(x_ticks, x_labels, rotation=90)
+    # eixo y
+    _, max_value = ax1.get_ylim()
+    max_value = int(math.ceil(math.log(max_value, 10)))
+    y_ticks = []
+    for i in range(max_value):
+        for j in [1, 2, 3, 5]:
+            y_ticks.append(j * 10 ** i)
+    ax1.yaxis.set_major_locator(ticker.FixedLocator(y_ticks))
+    ax1.yaxis.set_major_formatter(
+        ticker.FuncFormatter(lambda y, _: '{:g}'.format(y)))
+    # marcar valores relevantes
+    ax1.vlines(vlines_x, [0]*len(vlines_x), vlines_y,
+               linestyles='dashed', color='tab:grey', linewidth=1)
+    ax1.hlines(vlines_y, [-10]*len(vlines_x), vlines_x,
+               linestyles='dashed', color='tab:grey', linewidth=1)
+    ax1.set_xlim(left=-1)
     # cria novo eixo y para novos casos
     ax2 = ax1.twinx()
     # coloca o total de casos por cima
@@ -74,7 +117,7 @@ def plot(data, conf):
     ax1.patch.set_visible(False)
     # plot de novos casos
     color = 'tab:blue'
-    ax2.set_ylabel('Novos casos', color=color)
+    ax2.set_ylabel('Novos Casos', color=color)
     ax2.bar(dias, conf, color=color)
     ax2.tick_params(axis='y', labelcolor=color)
     ax2.set_ylim(0, 10)
@@ -82,6 +125,7 @@ def plot(data, conf):
     # adicionar nota sobre último dia
     fig.tight_layout()  # otherwise the right y-label is slightly clipped
     plt.show()
+    fig.savefig('../img/confirmados-' + data[-1] + '.png')
 
 
 if __name__ == '__main__':
